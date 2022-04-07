08e2651e5ec7a622736486d5fe408bf392168aff
ht900929 <ht900929@fh-muenster.de>
2022-04-07 15:53:58 +0200

    First commit

---
+++ src/test/java/com/searchbar/sweng/searchbar/SearchbarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.searchbar.sweng.searchbar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchbarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
