89fbcedfd016864be992669446b5cd788bf7cd18
ht900929 <ht900929@fh-muenster.de>
2022-04-15 19:20:58 +0200

    data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
 INSERT INTO food (id,name)VALUES(1,"Mehl");
 INSERT INTO food (id,name)VALUES(2,"Wasser");
 INSERT INTO unvertraeglichkeiten (id,histamine,fructose,lactose)VALUES(1,false,false,false);
-INSERT INTO rezepte (id,name,arbeitszeit,kochzeit,gesamtzeit,portionen,menueart,is_vegan,is_vegetarisch,unvertraeglichkeiten_id,rezept_menge,food_name,rezept_menge,food_name)
-VALUES(1,"Burger Brötchen",12,15,27,2,"FRÜHSTÜCK",TRUE,TRUE,1,"200 Gramm","Mehl","400 ML","Wasser");
+INSERT INTO rezepte (id,name,arbeitszeit,kochzeit,gesamtzeit,portionen,menueart,is_vegan,is_vegetarisch,unvertraeglichkeiten_id)
+VALUES(1,"Burger Brötchen",12,15,27,2,"FRÜHSTÜCK",TRUE,TRUE,1);
+INSERT INTO rezepte_mengen_mapping(rezepte_id,food_name,rezepte_menge) VALUES (1,"Mehl","200Gramm")
