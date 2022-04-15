0834b34d23ce1a07a8f66073ca04ef9004482802
ht900929 <ht900929@fh-muenster.de>
2022-04-15 19:24:39 +0200

    data schreibfehler

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,4 @@ INSERT INTO food (id,name)VALUES(2,"Wasser");
 INSERT INTO unvertraeglichkeiten (id,histamine,fructose,lactose)VALUES(1,false,false,false);
 INSERT INTO rezepte (id,name,arbeitszeit,kochzeit,gesamtzeit,portionen,menueart,is_vegan,is_vegetarisch,unvertraeglichkeiten_id)
 VALUES(1,"Burger Brötchen",12,15,27,2,"FRÜHSTÜCK",TRUE,TRUE,1);
-INSERT INTO rezepte_mengen_mapping(rezepte_id,food_name,rezepte_menge) VALUES (1,"Mehl","200Gramm")
+INSERT INTO rezept_mengen_mapping(rezepte_id,food_name,rezepte_menge) VALUES (1,"Mehl","200Gramm")
