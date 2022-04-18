08eea161eebaef7eb7c8bdc5a891260dd00c0038
ht900929 <ht900929@fh-muenster.de>
2022-04-18 16:36:58 +0200

    persistence tests and exception

---
+++ src/main/java/com/searchbar/sweng/searchbar/model/NoSuchRecipieException.java
@@ -0,0 +1,5 @@
+package com.searchbar.sweng.searchbar.model;
+
+public class NoSuchRecipieException extends RuntimeException{
+    public NoSuchRecipieException(String message){super(message);}
+}
---
+++ src/test/java/com/searchbar/sweng/searchbar/PersistenceTest.java
@@ -0,0 +1,28 @@
+package com.searchbar.sweng.searchbar;
+
+import com.searchbar.sweng.searchbar.model.Rezepte;
+import com.searchbar.sweng.searchbar.model.RezepteRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+public class PersistenceTest {
+    @Autowired
+    RezepteRepository rezepteRepository;
+
+    @Test
+    public void findAllShouldWork(){
+        List<Rezepte> rezepte = rezepteRepository.findAll("Fleisch");
+        assertEquals(1, rezepte.get(0).getId());
+        assertEquals(2, rezepte.get(1).getId());
+        assertEquals(3, rezepte.get(2).getId());
+        assertEquals(4, rezepte.get(3).getId());
+        assertEquals(5, rezepte.get(4).getId());
+    }
+}
