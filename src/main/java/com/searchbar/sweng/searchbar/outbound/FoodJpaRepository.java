1565d674137ee31d7f7fca297a79a7d4a46c1ccc
ht900929 <ht900929@fh-muenster.de>
2022-05-31 18:47:28 +0200

    controller tests(fail)

--- src/main/java/com/searchbar/sweng/searchbar/outbound/FoodJpaRepository.java
+++ src/main/java/com/searchbar/sweng/searchbar/outbound/FoodJpaRepository.java
@@ -1,12 +1,13 @@
 package com.searchbar.sweng.searchbar.outbound;
 
 import com.searchbar.sweng.searchbar.model.Food;
+import com.searchbar.sweng.searchbar.model.Repositories.FoodRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface FoodJpaRepository extends CrudRepository<Food,Integer> {
+public interface FoodJpaRepository extends CrudRepository<Food,Integer>, FoodRepository {
     Optional<Food> findById(int id);
 }
