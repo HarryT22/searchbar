b54f7456f25e2d14acdd4de56e0c45a4a5558f6d
ht900929 <ht900929@fh-muenster.de>
2022-06-08 17:02:07 +0200

    removed unneccesary dependency

--- src/test/java/com/searchbar/sweng/searchbar/EventConsumerTests.java
+++ src/test/java/com/searchbar/sweng/searchbar/EventConsumerTests.java
@@ -1,9 +1,5 @@
 package com.searchbar.sweng.searchbar;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.searchbar.sweng.searchbar.model.*;
-import com.searchbar.sweng.searchbar.model.Event.EventPublisher;
-import com.searchbar.sweng.searchbar.model.Event.RezeptAddedEvent;
 import com.searchbar.sweng.searchbar.model.Event.UserRegisteredEvent;
 import com.searchbar.sweng.searchbar.model.Service.EmailService;
 import org.junit.jupiter.api.Test;
@@ -11,21 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.cloud.stream.binder.test.InputDestination;
-import org.springframework.cloud.stream.binder.test.OutputDestination;
 import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
 import org.springframework.context.annotation.Import;
 import org.springframework.messaging.support.MessageBuilder;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @Import(TestChannelBinderConfiguration.class)
-public class EventTests {
+public class EventConsumerTests {
 
     @MockBean
     private EmailService emailService;
